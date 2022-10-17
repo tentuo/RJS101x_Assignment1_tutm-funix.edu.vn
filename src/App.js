175787f7d88174f191e8312afb6e8cdce16a654f
tutmfx15408@funix.edu.vn <tutmfx15408@funix.edu.vn>
2022-10-17 14:48:25 +0700

    frtlab

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import { Container, Col, Form, Row, FormGroup, Label, Input, Button } from 'reactstrap';
+import { Collapse, Nav, NavItem, NavLink } from 'reactstrap';
+import React, { Component } from 'react';
+import Menu from './components/MenuComponent';
+
+class App extends Component {
+  render() {
+    return (
+    <div className="App">
+      <Navbar dark color="primary">
+        <div className="container">
+          <NavbarBrand href="/">ristorant</NavbarBrand>
+        </div>
+      </Navbar>
+      <Menu />
+    </div>
+   );
+  }
+}
+
+export default App;
+
